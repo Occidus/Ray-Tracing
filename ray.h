61f7ccc3e4848795d946643abcdaf881568cf8fb
Lucas Everitt <lucas.everitt@gmail.com>
2020-07-06 14:35:17 -0500

    basic sphere

---
+++ ray.h
@@ -0,0 +1,22 @@
+#ifndef RAYH
+#define RAYH
+#include "linear.h"
+
+using namespace r3;
+
+class ray {
+public:
+  ray() {}
+  ray(const Vec3f &a, const Vec3f &b) {
+    A = a;
+    B = b;
+  }
+  Vec3f origin() const { return A; }
+  Vec3f direction() const { return B; }
+  Vec3f point_at_parameter(float t) const { return A + t * B; }
+
+  Vec3f A;
+  Vec3f B;
+};
+
+#endif
--- rt.cpp
+++ rt.cpp
@@ -1,17 +1,44 @@
-# include <iostream>
+#include <iostream>
+#include "linear.h"
+#include "ray.h"
+
+using namespace r3;
+
+bool hit_sphere(const Vec3f& center, float radius, const ray& r) {
+	Vec3f oc = r.origin() - center;
+	float a = Dot(r.direction(), r.direction());
+	float b = 2.0 * Dot(oc, r.direction());
+	float c = Dot(oc, oc) - radius*radius;
+	float discr = b*b - 4*a*c;
+	return (discr > 0);
+}
+
+Vec3f color(const ray& r) {
+	if (hit_sphere(Vec3f(0.0,0.0,-1.0), 0.5, r))
+		return Vec3f(1.0, 0.0, 0.0);
+	Vec3f unit_direction = Vec3f(r.direction().Normalized());
+	float t = 0.5*(unit_direction.y + 1.0);
+	return (1.0-t)*Vec3f(1.0, 1.0, 1.0) + t*Vec3f(0.5, 0.7, 1.0);
+}
 
 int main() {
 	int nx = 200;
 	int ny = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	Vec3f lower_left_corner = Vec3f(-2.0, -1.0, -1.0);
+	Vec3f horizontal = Vec3f(4.0, 0.0, 0.0);
+	Vec3f vertical = Vec3f(0.0, 2.0, 0.0);
+	Vec3f origin = Vec3f(0.0, 0.0, 0.0);
 	for (int j = ny-1; j>= 0; j--){
 		for (int i = 0; i< nx; i++){
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+			Vec3f col = color(r);
+			int ir = int(255.99*col.x);
+			int ig = int(255.99*col.y);
+			int ib = int(255.99*col.z);
+
 			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
 	}
