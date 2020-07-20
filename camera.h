c15d74b83e5fbb9b73214573ffd1d81d94aa76fb
Lucas Everitt <lucas.everitt@gmail.com>
2020-07-20 18:24:31 -0500

    Defocus blur

--- camera.h
+++ camera.h
@@ -3,10 +3,19 @@
 
 #include "ray.h"
 
+Vec3f random_in_unit_disk() {
+  Vec3f p;
+  do {
+    p = 2.0 * Vec3f(drand48(), drand48(), 0) - Vec3f(1, 1, 0);
+  } while (Dot(p, p) >= 1.0);
+  return p;
+}
+
 class camera {
 public:
-  camera(Vec3f lookfrom, Vec3f lookat, Vec3f vup, float vfov, float aspect) {
-    Vec3f u, v, w;
+  camera(Vec3f lookfrom, Vec3f lookat, Vec3f vup, float vfov, float aspect,
+         float aperture, float focus_dist) {
+    lens_radius = aperture / 2;
     float theta = ToRadians(vfov);
     float half_height = tan(theta / 2);
     float half_width = aspect * half_height;
@@ -14,20 +23,25 @@ public:
     w = (lookfrom - lookat).Normalized();
     u = vup.Cross(w).Normalized();
     v = w.Cross(u);
-    lower_left_corner = Vec3f(-half_width, -half_height, -1.0);
-    lower_left_corner = origin - half_width * u - half_height * v - w;
-    horizontal = 2 * half_width * u;
-    vertical = 2 * half_height * v;
+    lower_left_corner = origin - half_width * focus_dist * u -
+                        half_height * focus_dist * v - focus_dist * w;
+    // lower_left_corner = origin - half_width * u - half_height * v - w;
+    horizontal = 2 * half_width * focus_dist * u;
+    vertical = 2 * half_height * focus_dist * v;
   }
-  ray get_ray(float u, float v) {
-    return ray(origin,
-               lower_left_corner + u * horizontal + v * vertical - origin);
+  ray get_ray(float s, float t) {
+    Vec3f rd = lens_radius * random_in_unit_disk();
+    Vec3f offset = u * rd.x + v * rd.y;
+    return ray(origin + offset, lower_left_corner + s * horizontal +
+                                    t * vertical - origin - offset);
   }
 
   Vec3f origin;
   Vec3f lower_left_corner;
   Vec3f horizontal;
   Vec3f vertical;
+  Vec3f u, v, w;
+  float lens_radius;
 };
 
 #endif
--- rt.cpp
+++ rt.cpp
@@ -30,21 +30,93 @@ Vec3f color(const ray& r, hitable *world, int depth) {
 	}
 }
 
+hitable *random_scene() {
+	int n = 500;
+	hitable **list = new hitable*[n+1];
+	list[0] = new sphere(Vec3f(0,-1000,0), 1000, new lambertian(Vec3f(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -9; a < 9; a++) {
+		for (int b = -9; b < 9; b++) {
+			float choose_solid = drand48();
+			float choose_mat = drand48();
+			Vec3f center(a+ 0.9*drand48(),0.2,b+0.9*drand48());
+			if ((center-Vec3f(4,0.2,0)).Length() > 0.9) {
+				if (choose_solid < 0.65) { // sphere
+					if (choose_mat < 0.8) { // diffuse
+						list[i++] = new sphere(center, 0.2,
+							new lambertian(Vec3f(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+					}
+					else if (choose_mat < 0.95) { // metal
+						list[i++] = new sphere(center, 0.2,
+							new metal(Vec3f(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
+					}
+					else { // glass
+						list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+					}
+				}
+				else { // torus
+					Vec3f tcenter(center.x,center.y-0.125,center.z);
+					if (choose_mat < 0.8) { // diffuse
+						list[i++] = new torus(tcenter, 0.125, 0.075,
+							new lambertian(Vec3f(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+					}
+					else if (choose_mat < 0.95) { // metal
+						list[i++] = new torus(tcenter, 0.125, 0.075,
+							new metal(Vec3f(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
+					}
+					else { // glass
+						list[i++] = new torus(tcenter, 0.125, 0.075, new dielectric(1.5));
+					}
+				}
+			}
+		}
+
+		/*list[i++] = new sphere(Vec3f(0, 1, 0), 1.0, new dielectric(1.5));
+		list[i++] = new sphere(Vec3f(-4, 1, 0), 1.0, new lambertian(Vec3f(0.4, 0.2, 0.1)));
+		list[i++] = new sphere(Vec3f(4, 1, 0), 1.0, new metal(Vec3f(0.7, 0.6, 0.5), 0.0));
+
+		return new hitable_list(list,i);*/
+
+	}
+
+	list[i++] = new sphere(Vec3f(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(Vec3f(-4, 1, 0), 1.0, new lambertian(Vec3f(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(Vec3f(4, 1, 0), 1.0, new metal(Vec3f(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list,i);
+}
+
 int main() {
-	int nx = 600;
-	int ny = 300;
+	int nx = 150;
+	int ny = 100;
 	int ns = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	hitable *list[7];
+	
+	/*hitable *list[10];
 	list[0] = new sphere(Vec3f(0,-0.25,-1), 0.25, new lambertian(Vec3f(0.1, 0.2, 0.5)));
-	list[1] = new sphere(Vec3f(0,-100.5,-1), 100, new lambertian(Vec3f(0.8, 0.8, 0.0)));
-	list[2] = new sphere(Vec3f(-0.55,-0.25,-1), 0.25, new dielectric(1.5));
-	list[3] = new sphere(Vec3f(-0.55,-0.25,-1), -0.2, new dielectric(1.5));
-	list[4] = new sphere(Vec3f(0.55,-0.25,-1), 0.25, new metal(Vec3f(0.8, 0.6, 0.2), 0.1));
-	list[5] = new torus(Vec3f(-1.2, -0.4,-1), 0.15, 0.1, new metal(Vec3f(0.2, 0.6, 0.9), 0.1));
-	list[6] = new torus(Vec3f(1.2,-0.4,-1), 0.15, 0.1, new dielectric(1.01));
-	hitable *world = new hitable_list(list,7);
-	camera cam(Vec3f(-1.5,2,1), Vec3f(-0.5,0,-0.7), Vec3f(-0.3,1,0), 25, float(nx)/float(ny));
+	list[1] = new sphere(Vec3f(0.5,-0.25,-1.5), 0.25, new lambertian(Vec3f(0.1, 0.2, 0.5)));
+	list[2] = new sphere(Vec3f(-0.5,-0.25,-1.5), 0.25, new lambertian(Vec3f(0.1, 0.2, 0.5)));
+	list[3] = new sphere(Vec3f(0,-100.5,-1), 100, new lambertian(Vec3f(0.8, 0.8, 0.0)));
+	list[4] = new sphere(Vec3f(-0.55,-0.25,-1), 0.25, new dielectric(1.5));
+	list[5] = new sphere(Vec3f(-0.55,-0.25,-1), -0.2, new dielectric(1.5));
+	list[6] = new sphere(Vec3f(0.55,-0.25,-1), 0.25, new metal(Vec3f(0.8, 0.6, 0.2), 0.1));
+	list[7] = new torus(Vec3f(1.2,-0.4,-1), 0.15, 0.1, new lambertian(Vec3f(0.1, 0.8, 0.3)));
+	list[8] = new torus(Vec3f(-1.2, -0.4,-1), 0.15, 0.1, new metal(Vec3f(0.2, 0.6, 0.9), 0.1));
+	list[9] = new torus(Vec3f(-0.9, -0.4,-1.5), 0.15, 0.1, new dielectric(1.01));
+	hitable *world = new hitable_list(list,10);
+	
+	Vec3f lookfrom(0,0.75,0.5);
+	Vec3f lookat(0,0,-1);
+	Vec3f vup(0,1,0);
+	float dist_to_focus = (lookfrom-lookat).Length();
+	float aperture = 0.01;*/
+	hitable *world = random_scene();
+	Vec3f lookfrom(9,1.75,2.25);
+	Vec3f lookat(0,0,-0.65);
+	Vec3f vup(0,1,0);
+	float dist_to_focus = (lookfrom-lookat).Length();
+	float aperture = 0.1;
+	camera cam(lookfrom, lookat, vup, 35, float(nx)/float(ny), aperture, dist_to_focus);
 	for (int j = ny-1; j>= 0; j--){
 		for (int i = 0; i< nx; i++){
 			Vec3f col = Vec3f(0, 0, 0);
